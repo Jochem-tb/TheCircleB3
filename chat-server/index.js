18a3362f9833804a2bbc828e0d36cd78fae5b779
Jochem-tb <88931096+Jochem-tb@users.noreply.github.com>
2025-06-10 13:42:40 +0200

    Initial skeleton update

--- chat-server/index.js
+++ chat-server/index.js
@@ -1,5 +1,26 @@
-const express = require("express");
-const WebSocket = require("ws");
+import express from "express";
+import WebSocket from "ws";
+import http from "http";
 const app = express();
-const server = require("http").createServer(app);
+const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
+
+wss.on("connection", function connection(ws, req) {
+    const streamId = new URL(
+        req.url,
+        `http://${req.headers.host}`
+    ).pathname.slice(1);
+
+    if (!chatRooms[streamId]) chatRooms[streamId] = new Set();
+    chatRooms[streamId].add(ws);
+
+    ws.on("message", function incoming(message) {
+        for (const client of chatRooms[streamId]) {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(message);
+            }
+        }
+    });
+
+    ws.on("close", () => chatRooms[streamId].delete(ws));
+});
--- frontend/projects/viewer/src/app/app.component.ts
+++ frontend/projects/viewer/src/app/app.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    imports: [RouterOutlet],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'viewer';
+export class AppComponent implements OnInit {
+    ngOnInit(): void {
+        console.log('AppComponent initialized');
+        // You can add any initialization logic here
+    }
+    title = 'viewer';
 }
