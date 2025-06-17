ab47195faa06d93312b7ce39db02a88d12939bdf
Jochem-tb <88931096+Jochem-tb@users.noreply.github.com>
2025-06-17 10:24:40 +0200

    Create new layout for streaming/ingest

---
+++ ingest-server/src_old/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import config from "./config.js";
+import { startMediasoup } from "./mediasoupManager.js";
+import whipRouter from "./whipRouter.js";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.text({ type: "application/sdp" })); // for raw SDP
+app.use(cors({ origin: "*" }));
+
+app.use("/whip", whipRouter);
+
+app.listen(config.httpPort, async () => {
+    console.log(`Ingest API listening on http://0.0.0.0:${config.httpPort}`);
+    await startMediasoup();
+});
