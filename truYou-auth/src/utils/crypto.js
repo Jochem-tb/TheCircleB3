a566d15fb2f420cbdd848a48281c5190d0736c60
Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
2025-06-18 17:58:16 +0200

    Replacing crypto file

--- truYou-auth/src/utils/crypto.js
+++ truYou-auth/src/utils/crypto.js
@@ -1,10 +1,10 @@
-import crypto from 'crypto';
+const crypto = require('crypto');
 
 // Verifies a signature using the sender's public key.
 // message: string that was signed
 // publicKey: PEM formatted public key string
 // signature: base64 signature string
-export function verifySignature(message, publicKey, signature) {
+function verifySignature(message, publicKey, signature) {
     const verify = crypto.createVerify('SHA256');
     verify.update(message);
     verify.end();
@@ -14,3 +14,4 @@ export function verifySignature(message, publicKey, signature) {
         return false;
     }
 }
+module.exports = verifySignature;
