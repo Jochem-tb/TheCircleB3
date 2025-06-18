f1e14afa1c16eb07f14529a935c870a32327f695
Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
2025-06-18 18:37:20 +0200

    Index fix

--- truYou-auth/index.js
+++ truYou-auth/index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const verifySignature = require('./src/utils/crypto.js');
+const verifySignature = require('./src/utils/verifySignature.js');
 const errorHandler = require('./src/utils/errorHandler.js');
 const logger = require('./src/utils/logger.js');
 const authRoutes = require('./src/routes/auth.routes.js');
@@ -31,6 +31,11 @@ app.use(errorHandler);
 
 const port = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-    logger.info(`Auth server running on port ${port}`);
-});
+// Start alleen de server als het direct wordt uitgevoerd
+if (require.main === module) {
+    app.listen(port, () => {
+        logger.info(`Auth server running on port ${port}`);
+    });
+}
+
+module.exports = app;
