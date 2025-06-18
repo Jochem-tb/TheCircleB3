313c2b69c38ce4f57c2a6cf77fc70d60ec5e69a8
Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
2025-06-18 18:37:08 +0200

    Auth route update

--- truYou-auth/src/routes/auth.routes.js
+++ truYou-auth/src/routes/auth.routes.js
@@ -2,4 +2,7 @@ const express = require('express');
 const authController = require('../controllers/auth.controller');
 const authRoutes = express.Router();
 
+authRoutes.get('/challenge', authController.getChallenge);
+authRoutes.post('/authenticate', authController.postAuthenticate);
+
 module.exports = authRoutes;
